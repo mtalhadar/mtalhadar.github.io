5020f2f66853f6563824f3a1ba9243a908bec242
Muhammad Talha Dar <tdtalhadar@gmail.com>
2021-04-28 23:59:39 +0500

    aaaa

--- js-practice/educative.io/oop.js
+++ js-practice/educative.io/oop.js
@@ -1,7 +1,28 @@
-var employee={
-    name:'dar',
-    age:'12',
-    display(){
-        console.log(`mname is {this.name}`);
+//creating an object named employee
+var employee = {
+  firstName: 'Joe',
+  lastName: 'Adams',
+  age: 23,
+  sex: 'male',
+  designation: 'chef'
+};
+
+
+var product = {
+  name: 'Cheese',
+  price: 20,
+  amount: 10,
+  madeIn: 'USA'
+};
+
+function set(property, value, obj) {
+  for (let x in obj) {
+    if (x == property) {
+      obj[property] = value;
     }
+  }
 }
+
+set("name", "lmao", product);
+
+console.log(product);
